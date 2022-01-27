8c9049307befdc970d79a9ab7f08bece48112f02
ADMIN <ADMIN@DESKTOP-HB5GKS6>
2022-01-26 23:08:26 -0500

    sentencias de control paul

---
+++ src/ec/edu/intsuperior/controlador/pais.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.intsuperior.controlador;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class pais {
+
+    public pais(String p001, String argentina) {
+        
+    
+}
+}
---
+++ src/ec/edu/intsuperior/vista/aplicasion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.intsuperior.vista;
+import ec.edu.intsuperior.controlador.pais;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class aplicasion {
+    public static void main(String[] args) {
+        pais pais1= new pais("p001","Argentina");
+        pais pais2= new pais("p001","Brasil"); 
+        pais pais3= new pais("p001","Bolivia");
+        pais pais4= new pais("p001","Chile");   
+    
+}
+    }
