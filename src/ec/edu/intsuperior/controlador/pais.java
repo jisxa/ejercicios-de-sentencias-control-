c0061f10b0b849fa802bfd509254dc26309a4a0f
ADMIN <ADMIN@DESKTOP-HB5GKS6>
2022-01-26 23:20:42 -0500

    sentencias de control paul2.1

--- src/ec/edu/intsuperior/controlador/pais.java
+++ src/ec/edu/intsuperior/controlador/pais.java
@@ -9,9 +9,59 @@ package ec.edu.intsuperior.controlador;
  * @author ADMIN
  */
 public class pais {
+private String codigoPais;
+    private String nombrePais;
 
-    public pais(String p001, String argentina) {
-        
+    public pais(String codigoPais, String nombrePais) {
+        this.codigoPais = codigoPais;
+        this.nombrePais = nombrePais;
+    }
+
+    public pais() {
+       
+    }
+
+    public String getCodigoPais() {
+        return codigoPais;
+    }
+
+    public void setCodigoPais(String codigoPais) {
+        this.codigoPais = codigoPais;
+    }
+
+    public String getNombrePais() {
+        return nombrePais;
+    }
+
+    public void setNombrePais(String nombrePais) {
+        this.nombrePais = nombrePais;
+    }
+ 
     
-}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+    
+    
+    
+    
+    
+    
+    
 }
